ef629904d6c5bafe59ebefbd40fd69b6ef160aec
s1lugron <s1lugron@uni-trier.de>
2025-05-03 11:05:58 +0200

    some changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
+
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("green", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
-
+        //Hello
         Car car4 = new Car("yellow", "Renault", 4);
-        Car car5 = new Car("green", "Ferrari", 2);
+        Car car7 = new Car("green", "Ferrari", 2);
 
         //An dieser Stelle befindet sich nun ein Array
         //Dieses Array wird mit einem Sortieralgorithmus sortiert
