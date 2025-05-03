b9c6bf26f1e1fda142aa6706f466dfa411df58ee
s1lugron <s1lugron@uni-trier.de>
2025-05-03 10:53:26 +0200

    new car added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Car car3 = new Car("blue", "VW", 3);
 
         Car car4 = new Car("yellow", "Renault", 4);
+        Car car5 = new Car("green", "Ferrari", 2);
 
         //An dieser Stelle befindet sich nun ein Array
         //Dieses Array wird mit einem Sortieralgorithmus sortiert
